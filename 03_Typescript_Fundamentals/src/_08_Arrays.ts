d980d1c004c9752dd10d95e41c5fab781407bf87
Daniel B. OUATTARA <daniel.ouattara@gmx.com>
2024-03-29 22:46:43 +0100

    08 Arrays

---
+++ 03_Typescript_Fundamentals/src/_08_Arrays.ts
@@ -0,0 +1,17 @@
+export {};
+
+let prices: number[] = [100, 75, 42];
+prices.push(1);
+
+let fruit: string[] = ["apple", "orange"];
+fruit.pop();
+
+// caution
+
+// let peole: string[] = ["John", "Anna", 1]; // X incorrect
+
+// let randomValues: [] = [1]; // X incorrect
+
+let names = ["Peter", "Susan", 1];
+
+let array: (string | boolean)[] = ["apple", true, "orange", false, false];

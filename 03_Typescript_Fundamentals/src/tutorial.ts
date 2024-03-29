af36a2ce584388ce50fb8f21a090f9a029a0d33a
Daniel B. OUATTARA <daniel.ouattara@gmx.com>
2024-03-29 09:13:07 +0100

    03 Typescript fundamentals: type annotation + inference

---
+++ 03_Typescript_Fundamentals/src/tutorial.ts
@@ -0,0 +1,26 @@
+/**
+ * Typescript Fundamental
+ * ========================
+ */
+
+/* type annotations
+-------------------- */
+
+let awesome: string = "shakeAndBake";
+
+awesome = "Super ShakeAndBake";
+awesome = awesome.toUpperCase();
+console.log(awesome);
+// awesome = 20 // X Incorrect
+
+/*  */
+
+let amount: number = 20;
+amount = 12 - 1;
+//amount = "pants"; // X Incorrect;
+
+/*  */
+
+let isAwesome = true;
+isAwesome = false;
+// isAwesome = "shakeAndBake" // X Incorrect

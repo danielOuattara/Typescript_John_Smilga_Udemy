25c2b2b71aa2ee000fa0d76f738357ce4d16c1db
Daniel B. OUATTARA <daniel.ouattara@gmx.com>
2024-03-29 09:18:01 +0100

    03 Typescript fundamentals: 01 type annotations

---
+++ 03_Typescript_Fundamentals/src/_01_type_annotations.ts
@@ -0,0 +1,35 @@
+export {};
+
+/* --------------- type annotations  --------------- */
+
+let awesome: string = "shakeAndBake";
+
+awesome = "Super ShakeAndBake";
+awesome = awesome.toUpperCase();
+console.log(awesome);
+// awesome = 20 // X Incorrect
+
+/*  */
+
+let amount: number = 20;
+amount = 12 - 1;
+//amount = "pants"; // X Incorrect;
+
+/*  */
+
+let isAwesome = true;
+isAwesome = false;
+// isAwesome = "shakeAndBake" // X Incorrect
+
+/* type infer
+-------------------- */
+
+interface ISomeInterface {
+  name: string;
+  id: number;
+}
+
+let someObject: ISomeInterface = {
+  name: "random",
+  id: 123,
+};

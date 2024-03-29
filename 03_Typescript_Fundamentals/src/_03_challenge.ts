ea8474609c3aeecda4d549cf8c82cea0e507ea1d
Daniel B. OUATTARA <daniel.ouattara@gmx.com>
2024-03-29 09:24:22 +0100

    03 Typescript fundamentals: challenge

---
+++ 03_Typescript_Fundamentals/src/_03_challenge.ts
@@ -0,0 +1,18 @@
+/*-------------  challenge  ----------------- */
+
+let greetings: string = "Hello, Typescript";
+console.log(greetings);
+
+greetings.toLowerCase();
+console.log(greetings);
+
+let age: number = 40;
+console.log("age = ", age);
+age += 1;
+
+let isAdult: boolean = age > 21;
+console.log("isAdult = ", isAdult);
+
+// greetings = 10; // X incorrect
+// age = "forty"; // X incorrect
+// isAdult = "true"; // X incorrect

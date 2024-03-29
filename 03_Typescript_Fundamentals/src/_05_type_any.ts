6bdf9f5c89ca764c6fe68c27703f727ca73f05dd
Daniel B. OUATTARA <daniel.ouattara@gmx.com>
2024-03-29 10:06:36 +0100

    03 Typescript fundamentals: 05  type any

---
+++ 03_Typescript_Fundamentals/src/_05_type_any.ts
@@ -0,0 +1,8 @@
+export {};
+/*-------------  type any  ----------------- */
+
+// 'any' type makes code back to Javascript dynamic/loosely type
+
+let notSure: any = 4;
+notSure = true;
+notSure = "maybe";

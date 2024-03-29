f495a24d0fd0c359824439c4ad5729810f1ebb76
Daniel B. OUATTARA <daniel.ouattara@gmx.com>
2024-03-29 23:00:24 +0100

    10 objects

--- 03_Typescript_Fundamentals/src/_10_objects.ts
+++ 03_Typescript_Fundamentals/src/_10_objects.ts
@@ -1,1 +1,21 @@
 export {};
+
+let car: { brand: string; year: number } = { brand: "toyota", year: 2020 };
+car.brand = "ford";
+
+console.log(car);
+// car.color = "blue"; // X incorrect
+
+let car1: { brand: string; year: number } = { brand: "audi", year: 2021 };
+// let car2: { brand: string; year: number } = { brand: 'audi' };
+console.log(car1);
+
+let book = { title: "book", cost: 20 };
+let pen = { title: "pen", cost: 5 };
+let notebook = { title: "notebook" };
+
+let items: { readonly title: string; cost?: number }[] = [book, pen, notebook];
+console.log(items);
+
+// items[0].title = 'new book';  // X
+// Error: Cannot assign to 'title' because it is a read-only property

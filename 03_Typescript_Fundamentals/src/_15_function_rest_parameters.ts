0f275121feee92dd3d307bf5de4a38c8f3a9b3a8
Daniel B. OUATTARA <daniel.ouattara@gmx.com>
2024-04-01 21:02:54 +0200

    15 function rest parameter

---
+++ 03_Typescript_Fundamentals/src/_15_function_rest_parameters.ts
@@ -0,0 +1,16 @@
+export {};
+
+/*--- rest parameter ---*/
+
+function sum(message: string, ...numbers: number[]): string {
+  const doubled = numbers.map((num) => num * 2);
+  console.log(doubled);
+
+  let total = numbers.reduce((previous, current) => {
+    return previous + current;
+  }, 0);
+  return `${message} ${total}`;
+}
+
+let result = sum("The total is:", 1, 2, 3, 4, 5); // result will be "The total is: 15"
+console.log("result = ", result);

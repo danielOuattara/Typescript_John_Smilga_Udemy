0a6a76f249e12c544d2f2859b58784826a552185
Daniel B. OUATTARA <daniel.ouattara@gmx.com>
2024-04-05 07:35:50 +0200

    33 tuples

---
+++ 03_Typescript_Fundamentals/src/_33_tuples.ts
@@ -0,0 +1,40 @@
+export {};
+
+/*------- tuple ------- */
+
+let person: [string, number] = ["John", 25];
+console.log(person);
+
+let couple: [string, string] = ["John", "Marie"];
+console.log(couple);
+
+let date: [number, number, number] = [15, 12, 2085];
+console.log(date);
+
+function getPerson(): [string, number] {
+  return ["John", 25];
+}
+
+let somePerson = getPerson();
+console.log(somePerson[0]);
+console.log(somePerson[1]);
+
+/*------ gotcha ---------*/
+
+let employee: [string, number] = ["John", 25];
+console.log(employee);
+employee.unshift("Travolta");
+console.log(employee);
+employee.push(2674474467474);
+console.log(employee);
+
+/*--------- solution  -------*/
+
+let manager: readonly [string, number] = ["Anna", 25];
+console.log(manager);
+// manager.push("Neslon"); // X :  Property 'push' does not exist on type 'readonly [string, number]'.ts(2339)
+
+/*------ optional parameter in tuple -------*/
+
+let susan: [string, number?] = ["Susan"];
+console.log(susan);

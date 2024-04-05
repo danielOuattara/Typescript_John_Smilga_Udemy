71fc3f7dee74e3dc2f4d141c6a5c6405eda3c955
Daniel B. OUATTARA <daniel.ouattara@gmx.com>
2024-04-05 11:28:46 +0200

    39 type never

---
+++ 03_Typescript_Fundamentals/src/_39_type_never.ts
@@ -0,0 +1,65 @@
+export {};
+
+/**
+ * In TypeScript, `never` is a type that represents
+ * the type of values that never occur.
+ * You can't assign any value to a variable of type never.
+ * TypeScript will give a compile error if there are any
+ * unhandled cases, helping ensure that all cases are handled.
+ */
+
+/**Type 'number' is not assignable to type 'never'.ts(2322)*/
+// let someValue: never = 0; // X : incorrect
+
+// acceptable
+let neverValue: never;
+
+//----
+
+type Theme = "light" | "dark";
+
+function checkTheme(theme: Theme) {
+  if (theme === "light") {
+    console.log("light theme");
+    return;
+  }
+
+  if (theme === "dark") {
+    console.log("dark theme");
+    return;
+  }
+  /**
+   * theme below is of type `never`, because it can never
+   * have a value that is not 'light' or 'dark'.
+   */
+  console.log(theme); // type = `never`
+}
+
+checkTheme("light");
+checkTheme("dark");
+
+//------------------------------------
+
+enum EnumColor {
+  Red,
+  Blue,
+  // Green,
+}
+
+function getColorName(color: EnumColor) {
+  switch (color) {
+    case EnumColor.Red:
+      return "Red";
+    case EnumColor.Blue:
+      return "Blue";
+    default:
+      // at build time
+      let unexpectedColor: never = color;
+      // at runtime
+      throw new Error(`Unexpected color value: ${unexpectedColor}`);
+  }
+}
+
+console.log(getColorName(EnumColor.Red)); // Red
+console.log(getColorName(EnumColor.Blue)); // Blue
+// console.log(getColorName(EnumColor.Green)); // Green

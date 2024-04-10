167449c6ad0823ebbe6c3f9c408027ff3d165a41
Daniel B. OUATTARA <daniel.ouattara@gmx.com>
2024-04-10 23:57:39 +0200

    44 challenge truthy falsy guard

---
+++ 03_Typescript_Fundamentals/src/_44_challenge_truthy_falsy_guard.ts
@@ -0,0 +1,15 @@
+export {};
+/*------ challenge truthy falsy guard.ts ------*/
+
+function printLength(str: string | null | undefined) {
+  if (str) {
+    console.log(str.length);
+  } else {
+    console.log(`No string provided`);
+  }
+}
+
+printLength("Hello");
+printLength("");
+printLength(null);
+printLength(undefined);

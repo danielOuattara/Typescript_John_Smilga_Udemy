90f506dbd3dc77b10e7f38970fa5846c9ebc7fb3
Daniel B. OUATTARA <daniel.ouattara@gmx.com>
2024-04-12 19:54:44 +0200

    59 challenge fetch data Zod & Valibot libraries

---
+++ 03_Typescript_Fundamentals/src/courses/_59_challenge_fetch_data_and_Zod_library.ts
@@ -0,0 +1,51 @@
+export {};
+/*------ challenge fetch data and Zod library------
+
+Helps on types, also on runtime
+
+*/
+
+import { z } from "zod";
+
+const url = "https://www.course-api.com/react-tours-project";
+
+const tourSchema = z.object({
+  id: z.string(),
+  name: z.string(),
+  info: z.string(),
+  image: z.string(),
+  price: z.string(),
+  //...etc
+});
+
+// extract the inferred type
+type TypeTour = z.infer<typeof tourSchema>;
+
+async function fetchData(url: string): Promise<TypeTour[]> {
+  try {
+    const res = await fetch(url);
+    if (!res.ok) {
+      throw new Error(`HTTP error! status: 
+      ${res.status} 
+      ${res.statusText}`);
+    }
+    const rawData: TypeTour[] = await res.json();
+    const result = tourSchema.array().safeParse(rawData);
+
+    if (!result.success) {
+      throw new Error(`Invalid data: ${result.error}`);
+    }
+
+    return result.data;
+  } catch (err) {
+    const errMsg = err instanceof Error ? err.message : "there was an error...";
+    console.error(errMsg);
+    // throw error;
+    return [];
+  }
+}
+
+const tours = await fetchData(url);
+tours.map((tour) => {
+  console.log(tour.name);
+});
---
+++ 03_Typescript_Fundamentals/src/courses/_60_challenge_fetch_data_and_Valibot_library.ts
@@ -0,0 +1,48 @@
+export {};
+
+/*------ challenge fetch data and Valibot library------
+
+Helps on types, also on runtime
+
+*/
+
+import { array, object, Output, parse, string } from "valibot"; // 1.54 kB
+
+const url = "https://www.course-api.com/react-tours-project";
+
+const tourSchema = object({
+  id: string(),
+  name: string(),
+  info: string(),
+  image: string(),
+  price: string(),
+  //...etc
+});
+
+// extract the inferred type
+type TypeTour = Output<typeof tourSchema>;
+
+async function fetchData(url: string): Promise<TypeTour[]> {
+  try {
+    const res = await fetch(url);
+    if (!res.ok) {
+      throw new Error(`HTTP error! status: 
+      ${res.status} 
+      ${res.statusText}`);
+    }
+
+    const rawData: unknown = await res.json();
+    const result = parse(array(tourSchema), rawData);
+    return result;
+  } catch (err) {
+    const errMsg = err instanceof Error ? err.message : "there was an error...";
+    console.error(errMsg);
+    // throw error;
+    return [];
+  }
+}
+
+const tours = await fetchData(url);
+tours.map((tour) => {
+  console.log(tour.name);
+});

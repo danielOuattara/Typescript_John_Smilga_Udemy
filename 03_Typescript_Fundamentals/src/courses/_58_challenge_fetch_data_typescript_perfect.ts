70a09f6787d74eb611e421b13385aeaa038a8993
Daniel B. OUATTARA <daniel.ouattara@gmx.com>
2024-04-12 19:53:57 +0200

    58 challenge fetch data typescript perfect way

---
+++ 03_Typescript_Fundamentals/src/courses/_58_challenge_fetch_data_typescript_perfect.ts
@@ -0,0 +1,41 @@
+export {};
+/*------ challenge fetch data typescript perfect ------
+
+setup type and provide correct return type 
+
+*/
+
+interface ITour {
+  id: string;
+  name: string;
+  info: string;
+  image: string;
+  price: string;
+  //...etc
+}
+
+const url = "https://www.course-api.com/react-tours-project";
+
+async function fetchData(url: string): Promise<ITour[]> {
+  try {
+    const res = await fetch(url);
+    if (!res.ok) {
+      throw new Error(`HTTP error! status: 
+      ${res.status} 
+      ${res.statusText}`);
+    }
+
+    const data: ITour[] = await res.json();
+    return data;
+  } catch (err) {
+    const errMsg = err instanceof Error ? err.message : "there was an error...";
+    console.error(errMsg);
+    // throw error;
+    return [];
+  }
+}
+
+const tours = await fetchData(url);
+tours.map((tour) => {
+  console.log(tour.name);
+});

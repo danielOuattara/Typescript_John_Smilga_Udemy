c2289ed4b5c136ae542d670f1fb15637282f5a25
Daniel B. OUATTARA <daniel.ouattara@gmx.com>
2024-04-12 12:47:43 +0200

    47 type `never` gotcha

---
+++ 03_Typescript_Fundamentals/src/courses/_47_type_never_gotcha.ts
@@ -0,0 +1,36 @@
+export {};
+/*------ type never gotcha ------*/
+
+type Student = {
+  name: string;
+  study: () => void;
+};
+
+type User = {
+  name: string;
+  login: () => void;
+};
+
+type Person = Student | User;
+
+const person: Person = {
+  name: "Anna",
+  study() {
+    console.log(`${name} is studying`);
+  },
+  login() {
+    console.log(`${name} is logging`);
+  },
+};
+
+function isStudent(person: Person): person is Student {
+  // return 'study' in person;
+  return (person as Student).study !== undefined;
+}
+
+if (isStudent(person)) {
+  person.study(); // This is safe because TypeScript knows that 'person' is a Student.
+} else {
+  // in this case person is type "never"
+  console.log(person);
+}

96bfa3e489fc5ab12b0323e3f71333f2a16e0b8e
Daniel B. OUATTARA <daniel.ouattara@gmx.com>
2024-04-12 19:53:29 +0200

    57 fetch data basic insufficient

---
+++ 03_Typescript_Fundamentals/src/courses/_57_fetch_data_basic_insufficient.ts
@@ -0,0 +1,33 @@
+export {};
+/*------ fetch data basics: insufficient way ------
+
+- typically axios OR React Query OR both
+- we won't set any state values
+
+*/
+
+const url = "https://www.course-api.com/react-tours-project";
+
+async function fetchData(url: string) {
+  try {
+    const res = await fetch(url);
+    if (!res.ok) {
+      throw new Error(`HTTP error! status: 
+      ${res.status} 
+      ${res.statusText}`);
+    }
+
+    const data = await res.json();
+    return data;
+  } catch (err) {
+    const errMsg = err instanceof Error ? err.message : "there was an error...";
+    console.error(errMsg);
+    // throw error;
+    return [];
+  }
+}
+
+const tours = await fetchData(url);
+tours.map((tour: any) => {
+  console.log(tour.name);
+});

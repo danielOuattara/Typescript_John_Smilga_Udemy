ccf477fae11c43360d0f18cc9e939a10f77f66d4
Daniel B. OUATTARA <daniel.ouattara@gmx.com>
2024-04-12 19:52:08 +0200

    54 generic inferred type & type constraints

---
+++ 03_Typescript_Fundamentals/src/courses/_54_generics_inferred_type_and_type_constraints.ts
@@ -0,0 +1,33 @@
+export {};
+/*------ generics inferred type ------*/
+
+function pair<T, U>(param1: T, param2: U): [T, U] {
+  return [param1, param2];
+}
+
+// explicit specify generic
+let result_1 = pair<number, string>(123, "Hello");
+
+// typescript auto infers generics type
+let result_2 = pair(123, "Hello");
+
+/*  
+
+const [name,setName] = useState('')
+
+const [products,setProducts] = useState<Product[]>([])
+*/
+
+/*------  generics type constraints -------
+
+Here: type constraint on the generic type T,
+generic type can be either a number or a string. */
+
+function processValue<T extends string | number>(value: T): T {
+  console.log(value);
+  return value;
+}
+
+processValue("hello");
+processValue(12);
+// processValue(true); X: incorrect

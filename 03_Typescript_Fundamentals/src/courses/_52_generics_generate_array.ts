742fe7b86ce81fe78532d3f332d05e50bf28ae97
Daniel B. OUATTARA <daniel.ouattara@gmx.com>
2024-04-12 19:51:06 +0200

    52 generic generate array

---
+++ 03_Typescript_Fundamentals/src/courses/_52_generics_generate_array.ts
@@ -0,0 +1,20 @@
+export {};
+/*------ generics generate array------*/
+
+function createStringArray(length: number, value: string): string[] {
+  let result: string[] = [];
+  result = Array(length).fill(value);
+  return result;
+}
+console.log(createStringArray(3, `Hello`));
+
+//--------
+
+function createArray<T>(length: number, value: T): Array<T> {
+  let result: Array<T>;
+  result = Array(length).fill(value);
+  return result;
+}
+
+console.log(createArray<string>(3, `Nice day !`));
+console.log(createArray<number>(3, 40));

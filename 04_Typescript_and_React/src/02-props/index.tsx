f6c29d7dc51dd286d0278e6095530c875e74fbca
Daniel B. OUATTARA <daniel.ouattara@gmx.com>
2024-04-17 01:00:42 +0200

    04 Typescript+React: 02 props

--- 04_Typescript_and_React/src/02-props/index.tsx
+++ 04_Typescript_and_React/src/02-props/index.tsx
@@ -1,8 +1,39 @@
-function Component() {
+// type TypeComponentProps = {
+//   name: string;
+//   id?: number;
+//   children?: React.ReactNode;
+// };
+
+// function Component({ name, id, children }: TypeComponentProps) {
+//   return (
+//     <div>
+//       <h2>{name}</h2>
+//       <h2>{id}</h2>
+//       {children}
+//     </div>
+//   );
+// }
+// export default Component;
+
+//---------------------------------------- OR
+
+import { PropsWithChildren } from "react";
+
+type TypeComponentProps = {
+  name: string;
+  id?: number;
+};
+
+function Component({
+  name,
+  id,
+  children,
+}: PropsWithChildren<TypeComponentProps>) {
   return (
     <div>
-      <h2>React & Typescript</h2>
-      <h2>Props</h2>
+      <h2>{name}</h2>
+      <h2>{id}</h2>
+      {children}
     </div>
   );
 }
--- 04_Typescript_and_React/src/App.tsx
+++ 04_Typescript_and_React/src/App.tsx
@@ -1,9 +1,14 @@
-import Component from "./01-return";
+// import Component from "./01-return";
+import Component from "./02-props";
 
 export default function App() {
   return (
     <main>
-      <Component />
+      <Component name="John Doe" id={123}>
+        Hello World ! I am a text children
+        <p>I'm a paragraph children</p>
+      </Component>
+      <Component name="Jana Doe" />
     </main>
   );
 }

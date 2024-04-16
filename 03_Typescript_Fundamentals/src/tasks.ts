75088b5fac8b6135e0da46303ed74cd2fd92941a
Daniel B. OUATTARA <daniel.ouattara@gmx.com>
2024-04-16 21:35:45 +0200

    03 typescript fundamentals: tasks projects: select element + submit event

--- 03_Typescript_Fundamentals/src/tasks.ts
+++ 03_Typescript_Fundamentals/src/tasks.ts
@@ -1,1 +1,24 @@
-console.log("Hello Tasks");
+type TypeTask = {
+  description: string;
+  isCompleted: boolean;
+};
+
+const tasks: TypeTask[] = [];
+
+const taskForm = document.querySelector<HTMLFormElement>(".form")!;
+const formInput = document.querySelector<HTMLInputElement>(".form-input")!;
+const taskListElement = document.querySelector<HTMLUListElement>(".list");
+
+taskForm.addEventListener("submit", (event) => {
+  event.preventDefault();
+  const taskLabel = formInput.value;
+  if (taskLabel) {
+    // add task to list
+    // render tasks
+    // update local storage
+
+    formInput.value = "";
+    return;
+  }
+  alert("Please enter a task description");
+});

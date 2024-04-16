ba53b71e151a904c100e6fc9f19241c329afa33b
Daniel B. OUATTARA <daniel.ouattara@gmx.com>
2024-04-16 21:47:39 +0200

    03 typescript fundamentals: tasks projects: `addTask` logic

--- 03_Typescript_Fundamentals/src/tasks.ts
+++ 03_Typescript_Fundamentals/src/tasks.ts
@@ -1,5 +1,5 @@
 type TypeTask = {
-  description: string;
+  label: string;
   isCompleted: boolean;
 };
 
@@ -9,11 +9,38 @@ const taskForm = document.querySelector<HTMLFormElement>(".form")!;
 const formInput = document.querySelector<HTMLInputElement>(".form-input")!;
 const taskListElement = document.querySelector<HTMLUListElement>(".list");
 
+//--------------------------------
+// just for reference on event type definition
+// function createTask(event: SubmitEvent) {
+//   event.preventDefault();
+//   const taskLabel = formInput.value;
+//   if (taskLabel) {
+//     // add task to list
+//     // render tasks
+//     // update local storage
+
+//     formInput.value = "";
+//     return;
+//   }
+//   alert("Please enter a task description");
+// }
+// taskForm.addEventListener("submit", createTask);
+
+//--------------------------------
+
+function addToTasks(task: TypeTask): void {
+  tasks.push(task);
+  console.log(tasks);
+  return;
+}
+
 taskForm.addEventListener("submit", (event) => {
   event.preventDefault();
   const taskLabel = formInput.value;
   if (taskLabel) {
     // add task to list
+    addToTasks({ label: taskLabel, isCompleted: false });
+
     // render tasks
     // update local storage
 
@@ -22,21 +49,3 @@ taskForm.addEventListener("submit", (event) => {
   }
   alert("Please enter a task description");
 });
-
-//--------------------------------
-// just for reference on event type definition
-function createTask(event: SubmitEvent) {
-  event.preventDefault();
-  const taskLabel = formInput.value;
-  if (taskLabel) {
-    // add task to list
-    // render tasks
-    // update local storage
-
-    formInput.value = "";
-    return;
-  }
-  alert("Please enter a task description");
-}
-
-taskForm.addEventListener("submit", createTask);

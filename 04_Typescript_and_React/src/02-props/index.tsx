efbcd9cece8af1d0beb6f1af1970fed0b0f9ca96
Daniel B. OUATTARA <daniel.ouattara@gmx.com>
2024-04-17 01:26:06 +0200

    04 Typescript+React: 02 props: add App( )

--- 04_Typescript_and_React/src/02-props/index.tsx
+++ 04_Typescript_and_React/src/02-props/index.tsx
@@ -38,3 +38,22 @@ function Component({
   );
 }
 export default Component;
+
+//------------------------
+
+/* 
+import Component from "./02-props";
+
+export default function App() {
+  return (
+    <main>
+      <Component name="John Doe" id={123}>
+        Hello World ! I am a text children
+        <p>I'm a paragraph children</p>
+      </Component>
+      <Component name="Jana Doe" />
+    </main>
+  );
+}
+
+*/

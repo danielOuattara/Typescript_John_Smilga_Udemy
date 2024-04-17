4a908b402646ac85af5a89c21832557daf1b4ad6
Daniel B. OUATTARA <daniel.ouattara@gmx.com>
2024-04-17 17:50:49 +0200

    04 Typescript+React: fetch data: useEffect + fetch API

--- 04_Typescript_and_React/src/08-fetch-data/index.tsx
+++ 04_Typescript_and_React/src/08-fetch-data/index.tsx
@@ -1,9 +1,45 @@
-function Component() {
+import { useState, useEffect, useCallback } from "react";
+const url = "https://www.course-api.com/react-tours-project";
+
+export default function Component() {
+  const [isLoading, setIsLoading] = useState<boolean>(false);
+  const [isError, setIsError] = useState<string | null>(null);
+  // more on Tours state
+
+  const fetchData = useCallback(async () => {
+    setIsLoading(true);
+    try {
+      const response = await fetch(url);
+      if (!response.ok) {
+        throw new Error(`Failed to fetch tours...`);
+      }
+
+      const rawData = await response.json();
+      console.log(rawData);
+    } catch (error) {
+      const message =
+        error instanceof Error ? error.message : "there was an error...";
+      setIsError(message);
+    } finally {
+      setIsLoading(false);
+    }
+  }, []);
+
+  useEffect(() => {
+    fetchData();
+  }, []);
+
+  if (isLoading) {
+    return <h3>Loading...</h3>;
+  }
+
+  if (isError) {
+    return <h3>Error: {isError}</h3>;
+  }
+
   return (
     <div>
-      <h2>React & Typescript</h2>
-      <h2>Fetch Data</h2>
+      <h2 className="mb-1">Tours</h2>
     </div>
   );
 }
-export default Component;
--- 04_Typescript_and_React/src/App.tsx
+++ 04_Typescript_and_React/src/App.tsx
@@ -4,7 +4,8 @@
 // import Component from "./04-events";
 // import Component from "./05-challenge";
 // import Component from "./06-context";
-import Component from "./07-reducers";
+// import Component from "./07-reducers";
+import Component from "./08-fetch-data";
 
 export default function App() {
   return (

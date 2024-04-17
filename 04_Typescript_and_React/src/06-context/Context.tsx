d4b101201a8a796de77a8e9af713616458328364
Daniel B. OUATTARA <daniel.ouattara@gmx.com>
2024-04-17 15:00:37 +0200

    04 Typescript+React: context: theme example

--- 04_Typescript_and_React/src/06-context/Context.tsx
+++ 04_Typescript_and_React/src/06-context/Context.tsx
@@ -1,14 +1,56 @@
-import { createContext, useContext } from "react";
+//---------------------------- Basic Example
 
-const ThemeContext = createContext<string | undefined>(undefined);
+// import { createContext, useContext } from "react";
 
-type TypeProps = {
-  children: React.ReactNode;
+// const ThemeContext = createContext<string | undefined>(undefined);
+
+// type TypeProps = {
+//   children: React.ReactNode;
+// };
+
+// export default function ThemeContextProvider({ children }: TypeProps) {
+//   return (
+//     <ThemeContext.Provider value={"hello"}>{children}</ThemeContext.Provider>
+//   );
+// }
+
+// export const useThemeContext = () => {
+//   const context = useContext(ThemeContext);
+
+//   if (context === undefined) {
+//     throw new Error(`context must be used inside ThemeContextProvider`);
+//   }
+
+//   return context;
+// };
+
+//---------------------------------------------
+
+import { createContext, useContext, useState } from "react";
+
+type TypeTheme = "light" | "dark" | "system";
+
+type TypeThemeContext = {
+  theme: TypeTheme | undefined;
+  setTheme: React.Dispatch<React.SetStateAction<TypeTheme>>;
 };
 
-export default function ThemeContextProvider({ children }: TypeProps) {
+const ThemeContext = createContext<TypeThemeContext | undefined>(undefined);
+
+type TypeThemeContextProviderProps = {
+  children: React.ReactNode;
+  defaultTheme?: TypeTheme;
+};
+
+export default function ThemeContextProvider({
+  children,
+  defaultTheme = "system",
+}: TypeThemeContextProviderProps) {
+  const [theme, setTheme] = useState<TypeTheme>(defaultTheme);
   return (
-    <ThemeContext.Provider value={"hello"}>{children}</ThemeContext.Provider>
+    <ThemeContext.Provider value={{ theme, setTheme }}>
+      {children}
+    </ThemeContext.Provider>
   );
 }
 
--- 04_Typescript_and_React/src/06-context/index.tsx
+++ 04_Typescript_and_React/src/06-context/index.tsx
@@ -15,8 +15,19 @@ export function Component() {
   console.log("context = ", context);
   return (
     <div>
-      <h2>React & Typescript</h2>
-      <h2>Context API</h2>
+      <h2>random component</h2>
+      <button
+        onClick={() => {
+          if (context.theme === "dark") {
+            context.setTheme("system");
+            return;
+          }
+          context.setTheme("dark");
+        }}
+        className="btn btn-center"
+      >
+        toggle theme
+      </button>
     </div>
   );
 }

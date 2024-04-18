4ccc68476129675a03a97e375c02d8f61805a5e6
Daniel B. OUATTARA <daniel.ouattara@gmx.com>
2024-04-18 15:11:27 +0200

    05: Comfy Sloth Store App: testing Link and Link in Button from shadcn/ui

--- 05_Comfy_Sloth_Store_App/src/pages/Cart.tsx
+++ 05_Comfy_Sloth_Store_App/src/pages/Cart.tsx
@@ -1,3 +1,16 @@
+import { Button } from "@/components/ui/button";
+import { Link } from "react-router-dom";
+
 export default function Cart() {
-  return <h1 className="text-4xl">Cart Page</h1>;
+  return (
+    <>
+      <h1 className="text-4xl">Cart Page</h1>
+      <Link to={"/"} className="text-3xl text-red-900">
+        back to home
+      </Link>
+      <Button asChild size={"lg"}>
+        <Link to={"/"}>home button</Link>
+      </Button>
+    </>
+  );
 }

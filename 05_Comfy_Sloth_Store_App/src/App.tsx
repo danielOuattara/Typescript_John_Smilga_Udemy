fca4a0d28b1ab2ca1c752a23f5958b66e6c1827d
Daniel B. OUATTARA <daniel.ouattara@gmx.com>
2024-04-18 15:53:01 +0200

    05: Comfy Sloth Store App: setup HomeLayout + Outlet + add all pages routes

--- 05_Comfy_Sloth_Store_App/src/App.tsx
+++ 05_Comfy_Sloth_Store_App/src/App.tsx
@@ -16,8 +16,21 @@ import * as Page from "./pages";
 import { RouterProvider, createBrowserRouter } from "react-router-dom";
 
 const router = createBrowserRouter([
-  { path: "/", element: <Page.Landing /> },
-  { path: "/cart", element: <Page.Cart /> },
+  {
+    path: "/",
+    element: <Page.HomeLayout />,
+    children: [
+      { index: true, element: <Page.Landing /> },
+      { path: "/products", element: <Page.Products /> },
+      { path: "/products/:productId", element: <Page.SingleProduct /> },
+      { path: "/cart", element: <Page.Cart /> },
+      { path: "/about", element: <Page.About /> },
+      { path: "/checkout", element: <Page.Checkout /> },
+      { path: "/orders", element: <Page.Orders /> },
+    ],
+  },
+  { path: "/login", element: <Page.Login /> },
+  { path: "/register", element: <Page.Register /> },
 ]);
 
 export default function App() {

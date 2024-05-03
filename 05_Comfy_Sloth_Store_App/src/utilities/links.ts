1bf50d99e5f7e7a40a309bd4a2c24de36dcc0b83
Daniel B. OUATTARA <daniel.ouattara@gmx.com>
2024-05-03 23:09:00 +0200

    05: Comfy Sloth Store App: add links in utilities

---
+++ 05_Comfy_Sloth_Store_App/src/utilities/links.ts
@@ -0,0 +1,13 @@
+type TypeLink = {
+  href: string;
+  label: string;
+};
+
+export const links: TypeLink[] = [
+  { href: "/", label: "home" },
+  { href: "about", label: "about" },
+  { href: "products", label: "products" },
+  { href: "cart", label: "cart" },
+  { href: "checkout", label: "checkout" },
+  { href: "orders", label: "orders" },
+];

5e952eefe02a6e01bf4bd028a1a9f8e2a1f22156
Daniel B. OUATTARA <daniel.ouattara@gmx.com>
2024-05-09 09:52:26 +0200

    05: Comfy Sloth Store App: Renamed component

--- 05_Comfy_Sloth_Store_App/src/components/index.tsx
+++ 05_Comfy_Sloth_Store_App/src/components/index.tsx
@@ -1,6 +1,6 @@
 export { default as CartButton } from "./CartButton";
 export { default as Header } from "./Header";
-export { default as LinksDropdown } from "./LinksDropdown";
+export { default as NavLinksDropdown } from "./NavLinksDropdown";
 export { default as Logo } from "./Logo";
 export { default as Navbar } from "./Navbar";
 export { default as NavLinks } from "./NavLinks";

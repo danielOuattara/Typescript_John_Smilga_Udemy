d25dce6631a4076d093916da8bf4dca2c82b5b56
Daniel B. OUATTARA <daniel.ouattara@gmx.com>
2024-05-09 09:15:48 +0200

    05: Comfy Sloth Store App: links updated

--- 05_Comfy_Sloth_Store_App/src/utilities/links.ts
+++ 05_Comfy_Sloth_Store_App/src/utilities/links.ts
@@ -5,9 +5,9 @@ type TypeLink = {
 
 export const links: TypeLink[] = [
   { href: "/", label: "home" },
-  { href: "about", label: "about" },
-  { href: "products", label: "products" },
-  { href: "cart", label: "cart" },
-  { href: "checkout", label: "checkout" },
-  { href: "orders", label: "orders" },
+  { href: "/about", label: "about" },
+  { href: "/products", label: "products" },
+  { href: "/cart", label: "cart" },
+  { href: "/checkout", label: "checkout" },
+  { href: "/orders", label: "orders" },
 ];

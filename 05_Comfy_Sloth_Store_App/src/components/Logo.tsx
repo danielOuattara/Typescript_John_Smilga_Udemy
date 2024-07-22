b0a8118614616e4a85ef4470511eee8738e6cc92
Daniel B. OUATTARA <daniel.ouattara@gmx.com>
2024-07-23 00:11:52 +0200

    05: Comfy Sloth Store App: Logo component

--- 05_Comfy_Sloth_Store_App/src/components/Logo.tsx
+++ 05_Comfy_Sloth_Store_App/src/components/Logo.tsx
@@ -1,3 +1,13 @@
+import { Link } from "react-router-dom";
+import { Armchair } from "lucide-react";
+
 export default function Logo() {
-  return <h2 className="text-4xl">Logo</h2>;
+  return (
+    <Link
+      to="/"
+      className="hidden lg:flex justify-center items-center bg-primary p-2 rounded-lg text-white"
+    >
+      <Armchair className="w-8 h-8" />
+    </Link>
+  );
 }

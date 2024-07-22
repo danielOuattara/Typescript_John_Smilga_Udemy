5c4781b785d435c240e100cf75a6c1874d091b6b
Daniel B. OUATTARA <daniel.ouattara@gmx.com>
2024-07-22 17:56:12 +0200

    05: Comfy Sloth Store App: Outlet component - Shared layout

--- 05_Comfy_Sloth_Store_App/src/App.tsx
+++ 05_Comfy_Sloth_Store_App/src/App.tsx
@@ -2,8 +2,29 @@ import { RouterProvider, createBrowserRouter } from "react-router-dom";
 import * as pages from "./pages";
 
 const router = createBrowserRouter([
-  { path: "/", element: <pages.Landing /> },
-  { path: "/cart", element: <pages.Cart /> },
+  {
+    path: "/",
+    element: <pages.HomeLayout />,
+    children: [
+      { index: true, element: <pages.Landing /> },
+      { path: "/products", element: <pages.Products /> },
+      { path: "/products/:productId", element: <pages.SingleProduct /> },
+      { path: "/cart", element: <pages.Cart /> },
+      { path: "/about", element: <pages.About /> },
+      { path: "/checkout", element: <pages.Checkout /> },
+      { path: "/orders", element: <pages.Orders /> },
+    ],
+  },
+  {
+    path: "/login",
+    element: <pages.Login />,
+    errorElement: <pages.Error />,
+  },
+  {
+    path: "/register",
+    element: <pages.Register />,
+    errorElement: <pages.Error />,
+  },
 ]);
 
 export default function App() {

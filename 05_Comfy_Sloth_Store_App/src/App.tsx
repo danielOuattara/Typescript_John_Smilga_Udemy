c428d16c41747ceae6dbb2a7e0166b685cd8e1db
Daniel B. OUATTARA <daniel.ouattara@gmx.com>
2024-07-22 13:43:41 +0200

    05: Comfy Sloth Store App: install shadcn/ui + config + update read_me_build.md

--- 05_Comfy_Sloth_Store_App/src/App.tsx
+++ 05_Comfy_Sloth_Store_App/src/App.tsx
@@ -1,3 +1,17 @@
+import { Button } from "@/components/ui/button";
+
 export default function App() {
-  return <h1 className="text-3xl font-bold">Comfy Store</h1>;
+  return (
+    <div>
+      <h1 className="text-3xl font-bold">Comfy Store</h1>
+      <Button
+        variant="destructive"
+        size="lg"
+        onClick={() => console.log("it worked!!!")}
+        className="m-5"
+      >
+        Click Me
+      </Button>
+    </div>
+  );
 }

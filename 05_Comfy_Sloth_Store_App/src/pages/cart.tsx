89529169f03daf3ac3a46da1fed1c50ac278f6fa
Daniel B. OUATTARA <daniel.ouattara@gmx.com>
2024-07-22 17:39:46 +0200

    05: Comfy Sloth Store App: Link component

--- 05_Comfy_Sloth_Store_App/src/pages/cart.tsx
+++ 05_Comfy_Sloth_Store_App/src/pages/cart.tsx
@@ -1,3 +1,19 @@
-export default function Cart() { 
-  return <h1 className="text-4xl">Cart Page</h1>; 
+import { Button } from "@/components/ui/button";
+import { Link } from "react-router-dom";
+
+export default function Cart() {
+  return (
+    <div>
+      <h1 className="text-4xl">Cart Page</h1>
+      <Link to={"/"} className="text-3xl text-red-300 m-5">
+        home Link
+      </Link>
+
+      <br />
+
+      <Button asChild size="lg" className="m-5">
+        <Link to={"/"}>Home</Link>
+      </Button>
+    </div>
+  );
 }

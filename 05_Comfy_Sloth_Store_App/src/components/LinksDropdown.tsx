f69e4e9144c5251e67a7db191ebf94151ba426bc
Daniel B. OUATTARA <daniel.ouattara@gmx.com>
2024-07-23 08:42:10 +0200

    05: Comfy Sloth Store App: Links dropdown component

--- 05_Comfy_Sloth_Store_App/src/components/LinksDropdown.tsx
+++ 05_Comfy_Sloth_Store_App/src/components/LinksDropdown.tsx
@@ -1,3 +1,43 @@
+import {
+  DropdownMenu,
+  DropdownMenuTrigger,
+  DropdownMenuContent,
+  DropdownMenuItem,
+} from "@/components/ui/dropdown-menu";
+
+import { Button } from "./ui/button";
+import { links } from "@/utilities";
+import { NavLink } from "react-router-dom";
+import { AlignLeft } from "lucide-react";
+
 export default function LinksDropdown() {
-  return <h2 className="text-4xl">LinksDropdown</h2>;
+  return (
+    <DropdownMenu>
+      <DropdownMenuTrigger asChild className="lg:hidden">
+        <Button variant={"outline"} size={"icon"}>
+          <AlignLeft />
+          <span className="sr-only">Toggle links</span>
+        </Button>
+      </DropdownMenuTrigger>
+
+      <DropdownMenuContent
+        className="w-52 lg:hidden"
+        align="start"
+        sideOffset={25}
+      >
+        {links.map((link) => (
+          <DropdownMenuItem key={link.label}>
+            <NavLink
+              to={link.href}
+              className={({ isActive }) =>
+                `capitalize w-full ${isActive ? "text-primary" : ""}`
+              }
+            >
+              {link.label}
+            </NavLink>
+          </DropdownMenuItem>
+        ))}
+      </DropdownMenuContent>
+    </DropdownMenu>
+  );
 }

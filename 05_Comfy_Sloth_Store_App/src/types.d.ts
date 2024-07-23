84788c4a615121efadb8d4dc9a06ff86a801b91e
Daniel B. OUATTARA <daniel.ouattara@gmx.com>
2024-07-23 18:39:19 +0200

    05: Comfy Sloth Store App: Product Types definition

--- 05_Comfy_Sloth_Store_App/src/types.d.ts
+++ 05_Comfy_Sloth_Store_App/src/types.d.ts
@@ -8,3 +8,39 @@ type Theme = "dark" | "light" | "system";
 type ThemeState = {
   theme: Theme;
 };
+
+type ProductsResponse = {
+  data: Product[];
+  meta: ProductsMeta;
+};
+
+type Product = {
+  id: number;
+  attributes: {
+    category: string;
+    company: string;
+    createdAt: string;
+    description: string;
+    featured: boolean;
+    image: string;
+    price: string;
+    publishedAt: string;
+    shipping: boolean;
+    title: string;
+    updatedAt: string;
+    colors: string[];
+  };
+};
+
+type ProductsMeta = {
+  categories: string[];
+  companies: string[];
+  pagination: Pagination;
+};
+
+type Pagination = {
+  page: number;
+  pageCount: number;
+  pageSize: number;
+  total: number;
+};

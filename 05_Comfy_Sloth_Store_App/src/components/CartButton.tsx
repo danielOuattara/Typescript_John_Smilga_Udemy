21200b4dbe1c8c49870689f0eaae0cdc658f950d
Daniel B. OUATTARA <daniel.ouattara@gmx.com>
2024-07-23 17:30:39 +0200

    05: Comfy Sloth Store App: CartButton component

--- 05_Comfy_Sloth_Store_App/src/components/CartButton.tsx
+++ 05_Comfy_Sloth_Store_App/src/components/CartButton.tsx
@@ -1,3 +1,22 @@
+import { Button } from "./ui/button";
+import { ShoppingCart } from "lucide-react";
+import { Link } from "react-router-dom";
+
 export default function CartButton() {
-  return <h2 className="text-4xl">CartButton</h2>;
+  const numberItemsInCart = 5;
+  return (
+    <Button
+      asChild
+      variant="outline"
+      size="icon"
+      className="flex justify-center items-center relative"
+    >
+      <Link to="/cart">
+        <ShoppingCart />
+        <span className="absolute -top-3 -right-3 bg-primary text-white rounded-full h-6 w-6 flex items-center justify-center text-xs">
+          {numberItemsInCart}
+        </span>
+      </Link>
+    </Button>
+  );
 }

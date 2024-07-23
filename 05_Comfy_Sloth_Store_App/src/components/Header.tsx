0606afa65f93167723e3e5300eb88129a330ab6e
Daniel B. OUATTARA <daniel.ouattara@gmx.com>
2024-07-23 09:53:05 +0200

    05: Comfy Sloth Store App: Correct Header component, forgot to update logic

--- 05_Comfy_Sloth_Store_App/src/components/Header.tsx
+++ 05_Comfy_Sloth_Store_App/src/components/Header.tsx
@@ -1,3 +1,38 @@
+import { useState } from "react";
+import { Link, useNavigate } from "react-router-dom";
+import { Button } from "./ui/button";
+
 export default function Header() {
-  return <h2 className="text-4xl">Header</h2>;
+  const navigate = useNavigate();
+  const [user, setUser] = useState<string | null>("demo user");
+
+  const handleLogout = () => {
+    navigate("/");
+    setUser(null);
+    return;
+  };
+
+  return (
+    <header>
+      <div className="align-element flex justify-center sm:justify-end py-2">
+        {user ? (
+          <div className="flex gap-x-2 sm:gap-x-8 items-center">
+            <p className="text-xs sm:text-sm">Hello {user}</p>
+            <Button variant={"link"} size={"sm"} onClick={handleLogout}>
+              Logout
+            </Button>
+          </div>
+        ) : (
+          <div className="flex gap-x-6 justify-center items-center -mr-4">
+            <Button asChild variant={"link"} size={"sm"}>
+              <Link to={"login"}>Sign in / Guest</Link>
+            </Button>
+            <Button asChild variant={"link"} size={"sm"}>
+              <Link to={"register"}>Register</Link>
+            </Button>
+          </div>
+        )}
+      </div>
+    </header>
+  );
 }

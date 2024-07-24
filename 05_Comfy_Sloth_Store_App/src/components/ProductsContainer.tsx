d4b1c77b18bed49a96523bb8ff139ade409595cd
Daniel B. OUATTARA <daniel.ouattara@gmx.com>
2024-07-24 11:54:43 +0200

    05: Comfy Sloth Store App: ProductsList component

--- 05_Comfy_Sloth_Store_App/src/components/ProductsContainer.tsx
+++ 05_Comfy_Sloth_Store_App/src/components/ProductsContainer.tsx
@@ -1,3 +1,5 @@
+import { ProductsList } from "./index";
+
 export default function ProductsContainer() {
-  return <h2 className="text-4xl">ProductsContainer</h2>;
+  return <ProductsList />;
 }
--- 05_Comfy_Sloth_Store_App/src/components/ProductsList.tsx
+++ 05_Comfy_Sloth_Store_App/src/components/ProductsList.tsx
@@ -1,6 +1,34 @@
-import { useLoaderData } from "react-router-dom";
+import { Link, useLoaderData } from "react-router-dom";
+import { formatMoney } from "@/utilities";
+import { Card, CardContent } from "./ui/card";
 
 export default function ProductsList() {
-  const res = useLoaderData() as ProductsResponse;
-  return <h2 className="text-4xl">ProductsList</h2>;
+  const { data: products } = useLoaderData() as ProductsResponse;
+
+  return (
+    <div className="mt-12 grid gap-y-8">
+      {products.map((product) => (
+        <Link key={product.id} to={`/products/${product.id}`}>
+          <Card>
+            <CardContent className="p-8 gap-y-4 grid md:grid-cols-3 ">
+              <img
+                src={product.attributes.image}
+                alt={product.attributes.title}
+                className="h-64 w-full md:h-48 md:w-48 rounded-md object-cover"
+              />
+              <div>
+                <h2 className="text-xl font-semibold capitalize">
+                  {product.attributes.title}
+                </h2>
+                <h4>{product.attributes.company}</h4>
+              </div>
+              <p className="text-primary md:ml-auto">
+                {formatMoney(product.attributes.price)}
+              </p>
+            </CardContent>
+          </Card>
+        </Link>
+      ))}
+    </div>
+  );
 }

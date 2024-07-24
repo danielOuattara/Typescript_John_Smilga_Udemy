41e7a0b4bbb3892e65061c49b4d1b64df804d715
Daniel B. OUATTARA <daniel.ouattara@gmx.com>
2024-07-24 09:05:24 +0200

    05: Comfy Sloth Store App:  Hero Carousel + add Carrousel component from shadcn/ui

--- 05_Comfy_Sloth_Store_App/src/components/HeroCarousel.tsx
+++ 05_Comfy_Sloth_Store_App/src/components/HeroCarousel.tsx
@@ -1,3 +1,46 @@
+import { Card, CardContent } from "@/components/ui/card";
+import {
+  Carousel,
+  CarouselContent,
+  CarouselItem,
+  CarouselNext,
+  CarouselPrevious,
+} from "@/components/ui/carousel";
+
+import hero1 from "../assets/hero1.webp";
+import hero2 from "../assets/hero2.webp";
+import hero3 from "../assets/hero3.webp";
+import hero4 from "../assets/hero4.webp";
+
+const carouselImages = [hero1, hero2, hero3, hero4];
+
 export default function HeroCarousel() {
-  return <h2 className="text-4xl">HeroCarousel</h2>;
+  return (
+    <div className="hidden lg:block">
+      <Carousel
+        opts={{
+          align: "start",
+          loop: true,
+        }}
+      >
+        <CarouselContent>
+          {carouselImages.map((image, index) => (
+            <CarouselItem key={index}>
+              <Card>
+                <CardContent className="p-2">
+                  <img
+                    src={image}
+                    alt="hero"
+                    className="w-full h-[24rem] rounded-md object-cover"
+                  />
+                </CardContent>
+              </Card>
+            </CarouselItem>
+          ))}
+        </CarouselContent>
+        <CarouselPrevious />
+        <CarouselNext />
+      </Carousel>
+    </div>
+  );
 }

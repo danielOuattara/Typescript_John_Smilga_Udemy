369943b4fe598e591bbc98b128ee8a68304bb865
Daniel B. OUATTARA <daniel.ouattara@gmx.com>
2024-07-29 10:48:57 +0200

    05: Comfy Sloth Store App: CartItemList: SecondColumn

--- 05_Comfy_Sloth_Store_App/src/components/CartItemColumns.tsx
+++ 05_Comfy_Sloth_Store_App/src/components/CartItemColumns.tsx
@@ -24,8 +24,30 @@ export const FirstColumn = ({
   />
 );
 
-export const SecondColumn = () => <h2>2nd column</h2>;
+export const SecondColumn = ({
+  title,
+  company,
+  productColor,
+}: {
+  title: string;
+  company: string;
+  productColor: string;
+}) => (
+  <div className="sm:ml-4 md:ml-12 sm:w-48">
+    <h3 className="capitalize font-medium">{title}</h3>
+    <h4 className="mt-2 capitalize text-sm">{company}</h4>
+    <p className="mt-4 text-sm capitalize flex items-center gap-x-2">
+      color :
+      <span
+        className="aspect-square rounded-md w-4 h-4 mr-2 "
+        style={{
+          backgroundColor: productColor,
+        }}
+      ></span>
+    </p>
+  </div>
+);
 
-export const ThirdColumn = () => <h2>3rd column</h2>;
+export const ThirdColumn = () => <div>3rd column</div>;
 
 export const FourthColumn = () => <h2>4th column</h2>;

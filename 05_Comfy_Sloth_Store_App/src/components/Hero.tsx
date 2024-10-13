3ed9144ed58e32ddf6f9b1685db142c411adb599
Daniel B. OUATTARA <daniel.ouattara@gmx.com>
2024-10-14 01:42:25 +0200

    05: Comfy Sloth Store App React-Ts: update title

--- 05_Comfy_Sloth_Store_App/src/components/Hero.tsx
+++ 05_Comfy_Sloth_Store_App/src/components/Hero.tsx
@@ -7,7 +7,7 @@ export default function Hero() {
     <section className="grid grid-cols-1 lg:grid-cols-2 gap-24 items-center">
       <div>
         <h1 className="max-w-2xl text-4xl font-bold tracking-tight sm:text-6xl ">
-          We're changing the way people shop.
+          Shopping with us is nature friendly.
         </h1>
 
         <p className="mt-8 max-w-xl text-lg leading-8">
@@ -18,7 +18,7 @@ export default function Hero() {
         </p>
 
         <Button asChild size="lg" className="mt-10">
-          <Link to="/products">Our Products</Link>
+          <Link to="/products">Visit Our Products</Link>
         </Button>
       </div>
       <HeroCarousel />

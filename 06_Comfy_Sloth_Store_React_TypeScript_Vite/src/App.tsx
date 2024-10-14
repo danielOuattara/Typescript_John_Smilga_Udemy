34f2be7c6f96573d1281a11c8b28ec2a1df61526
Daniel B. OUATTARA <daniel.ouattara@gmx.com>
2024-10-14 22:15:35 +0200

    06_Comfy_Sloth_Store_React_TypeScript_Vite: shadcn/ui install from docs + test

--- 06_Comfy_Sloth_Store_React_TypeScript_Vite/src/App.tsx
+++ 06_Comfy_Sloth_Store_React_TypeScript_Vite/src/App.tsx
@@ -1,3 +1,16 @@
+import { Button } from "./components/ui/button";
+
 export default function App() {
-  return <h1 className="text-3xl font-bold">Hello world!</h1>;
+  return (
+    <div>
+      <h1 className="text-3xl font-bold m-5">App</h1>
+      <Button
+        variant="destructive"
+        size="lg"
+        onClick={() => console.log("it worked!!!")}
+      >
+        Click Me
+      </Button>
+    </div>
+  );
 }

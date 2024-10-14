dc35107898e9b601128e142a75a038c0ba9a4d85
Daniel B. OUATTARA <daniel.ouattara@gmx.com>
2024-10-14 22:51:22 +0200

    06 Comfy Sloth Store React TypeScript Vite: redux toolkit typescript setup + test

--- 06_Comfy_Sloth_Store_React_TypeScript_Vite/src/App.tsx
+++ 06_Comfy_Sloth_Store_React_TypeScript_Vite/src/App.tsx
@@ -1,6 +1,9 @@
 import { Button } from "./components/ui/button";
+import { useAppSelector } from "./hooks";
 
 export default function App() {
+  const { name } = useAppSelector((state) => state.user);
+  console.log(name);
   return (
     <div>
       <h1 className="text-3xl font-bold m-5">App</h1>
---
+++ 06_Comfy_Sloth_Store_React_TypeScript_Vite/src/hooks.ts
@@ -0,0 +1,11 @@
+import { useDispatch, useSelector } from "react-redux";
+import type { TypedUseSelectorHook } from "react-redux";
+import type { RootState, AppDispatch } from "./store";
+
+// Use throughout your app instead of plain `useDispatch` and `useSelector`
+export const useAppDispatch: () => AppDispatch = useDispatch;
+export const useAppSelector: TypedUseSelectorHook<RootState> = useSelector;
+export type ReduxStore = {
+  getState: () => RootState;
+  dispatch: AppDispatch;
+};

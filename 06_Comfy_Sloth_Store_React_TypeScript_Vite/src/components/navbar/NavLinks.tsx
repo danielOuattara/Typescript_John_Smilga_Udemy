8ac5a27a3d324fb35e493d99b7b89ed1a9736799
Daniel B. OUATTARA <daniel.ouattara@gmx.com>
2024-10-18 15:08:50 +0200

    06 Comfy Sloth Store React TypeScript Vite: NavLink comp.

--- 06_Comfy_Sloth_Store_React_TypeScript_Vite/src/components/navbar/NavLinks.tsx
+++ 06_Comfy_Sloth_Store_React_TypeScript_Vite/src/components/navbar/NavLinks.tsx
@@ -1,3 +1,26 @@
+import { navLinks } from "@/utilities";
+import { NavLink } from "react-router-dom";
+
 export default function NavLinks() {
-  return <h2 className="text-xl">NavLinks</h2>;
+  return (
+    <div className="hidden lg:flex justify-center items-center gap-x-4">
+      {navLinks.map((link) => {
+        return (
+          <NavLink
+            key={link.label}
+            to={link.href}
+            className={({ isActive }) =>
+              `capitalize tracking-tighter ${isActive ? "text-primary" : ""}`
+            }
+          >
+            <div className="flex justify-between">
+              {link.icon}
+              &nbsp;
+              {link.label}
+            </div>
+          </NavLink>
+        );
+      })}
+    </div>
+  );
 }

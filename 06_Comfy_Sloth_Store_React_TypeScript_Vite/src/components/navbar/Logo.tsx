9264076a7bd9120d481bc0ae5e52e85acbc6e740
Daniel B. OUATTARA <daniel.ouattara@gmx.com>
2024-10-18 13:28:56 +0200

    06 Comfy Sloth Store React TypeScript Vite: Logo comp.

--- 06_Comfy_Sloth_Store_React_TypeScript_Vite/src/components/navbar/Logo.tsx
+++ 06_Comfy_Sloth_Store_React_TypeScript_Vite/src/components/navbar/Logo.tsx
@@ -1,3 +1,19 @@
+import { LampFloor, Sofa } from "lucide-react";
+import { Link } from "react-router-dom";
+
 export default function Logo() {
-  return <h2 className="text-xl">Logo</h2>;
+  return (
+    <Link
+      to="/"
+      className="hidden lg:flex justify-center items-center bg-primary p-2 rounded-lg text-white"
+    >
+      <LampFloor
+        size={32}
+        color="#ecba4d"
+        strokeWidth={1.25}
+        absoluteStrokeWidth
+      />
+      <Sofa size={32} color="#f8efe6" strokeWidth={2} absoluteStrokeWidth />
+    </Link>
+  );
 }

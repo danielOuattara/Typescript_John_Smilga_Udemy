2ec7f8627d01c786493a6513bb416e0b2c907377
Daniel B. OUATTARA <daniel.ouattara@gmx.com>
2024-10-18 11:32:51 +0200

    06 Comfy Sloth Store React TypeScript Vite: Link navigation component

--- 06_Comfy_Sloth_Store_React_TypeScript_Vite/src/pages/cart.tsx
+++ 06_Comfy_Sloth_Store_React_TypeScript_Vite/src/pages/cart.tsx
@@ -1,3 +1,21 @@
-export default function Cart() { 
-  return <h1 className="text-4xl">Cart Page</h1>; 
+import { Link } from "react-router-dom";
+import { Button } from "@/components/ui/button";
+
+export default function Cart() {
+  return (
+    <>
+      <h1 className="text-4xl">Cart Page</h1>
+      <div>
+        <Link to="/" className="text-3xl text-blue-500">
+          {" "}
+          back home
+        </Link>
+      </div>
+      <div>
+        <Button asChild>
+          <Link to="/"> back home</Link>
+        </Button>
+      </div>
+    </>
+  );
 }

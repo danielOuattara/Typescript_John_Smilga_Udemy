7d3b6372ebf5d9ec8deb42638e0194610db07062
Daniel B. OUATTARA <daniel.ouattara@gmx.com>
2024-10-18 12:33:40 +0200

    06 Comfy Sloth Store React TypeScript Vite: Header comp.

--- 06_Comfy_Sloth_Store_React_TypeScript_Vite/src/pages/homeLayout.tsx
+++ 06_Comfy_Sloth_Store_React_TypeScript_Vite/src/pages/homeLayout.tsx
@@ -1,9 +1,10 @@
+import { Header } from "@/components/header";
 import { Outlet } from "react-router-dom";
 
 export default function HomeLayout() {
   return (
     <>
-      <header>header</header>
+      <Header />
       <nav>navbar</nav>
       <Outlet />
       <footer>Footer</footer>

2bf52350a9cfcd84f2cbd434be32df88e9a0db22
Daniel B. OUATTARA <daniel.ouattara@gmx.com>
2024-10-18 11:52:52 +0200

    06 Comfy Sloth Store React TypeScript Vite: Outlet comp. and shared layout

--- 06_Comfy_Sloth_Store_React_TypeScript_Vite/src/App.tsx
+++ 06_Comfy_Sloth_Store_React_TypeScript_Vite/src/App.tsx
@@ -2,10 +2,27 @@ import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import * as pages from "./pages";
 
 const router = createBrowserRouter([
-  { path: "/", element: <pages.HomeLayout /> },
   {
-    path: "/cart",
-    element: <pages.Cart />,
+    path: "/",
+    element: <pages.HomeLayout />,
+    children: [
+      { index: true, element: <pages.Landing /> },
+      { path: "products", element: <pages.Products /> },
+      { path: "products/:productId", element: <pages.SingleProduct /> },
+      { path: "cart", element: <pages.Cart /> },
+      { path: "about", element: <pages.About /> },
+      { path: "checkout", element: <pages.Checkout /> },
+      { path: "orders", element: <pages.Orders /> },
+    ],
+  },
+
+  {
+    path: "/login",
+    element: <pages.Login />,
+  },
+  {
+    path: "/register",
+    element: <pages.Register />,
   },
 ]);
 
